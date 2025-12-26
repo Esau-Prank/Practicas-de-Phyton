d2bea7d08cf67ce6411e4be9b0dcf6cec25ced2f
Esau DLRS <ed127596@gmail.com>
2025-12-25 23:42:37 -0600

    AREA DE UN CIRCULO
    EASY

---
+++ AREA TR.py
@@ -0,0 +1,16 @@
+# Encontrar el area de un circulo
+# Datos que se piden
+
+'''
+R= ES EL RADIO
+P= ES PI QUE ES UNA CONSTANTE
+'''
+
+P=float(3.1416)
+R=int(input('Ingrese su radio: '))
+
+#Operacion
+
+Area=((R*R)*P)
+
+print('Su area de su cirulo es:',Area)
